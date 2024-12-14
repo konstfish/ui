f154ed097ffb0403a2aee3758c1ca1bec4b0e77d
David Fischer <david@konst.fish>
2024-12-14 23:40:53 +0100

    feat: poc

---
+++ core/element.go
@@ -0,0 +1,57 @@
+package ui
+
+import (
+	"bytes"
+	"html"
+	"text/template"
+)
+
+type Element struct {
+	Tag        string
+	Classes    []string
+	Attributes map[string]string
+	Content    interface{}
+	Children   []*Element
+}
+
+func NewElement(tag string) *Element {
+	return &Element{
+		Tag:        tag,
+		Classes:    make([]string, 0),
+		Attributes: make(map[string]string),
+		Children:   make([]*Element, 0),
+	}
+}
+
+func (e *Element) AddClass(class string) *Element {
+	e.Classes = append(e.Classes, class)
+	return e
+}
+
+func (e *Element) SetAttribute(key, value string) *Element {
+	e.Attributes[key] = value
+	return e
+}
+
+func (e *Element) SetContent(content string) *Element {
+	e.Content = html.EscapeString(content)
+	return e
+}
+
+func (e *Element) AddChild(child *Element) *Element {
+	e.Children = append(e.Children, child)
+	return e
+}
+
+var tmpl = template.Must(template.New("element").Parse(`
+{{define "element"}}<{{.Tag}}{{if .Classes}} class="{{range $i, $class := .Classes}}{{if $i}} {{end}}{{$class}}{{end}}"{{end}}{{range $key, $value := .Attributes}} {{$key}}="{{$value}}"{{end}}>{{if .Content}}{{.Content}}{{end}}{{range .Children}}{{template "element" .}}{{end}}</{{.Tag}}>{{end}}
+`))
+
+func (e *Element) Render() (string, error) {
+	var buf bytes.Buffer
+	err := tmpl.ExecuteTemplate(&buf, "element", e)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/konstfish/ui
+
+go 1.22.5
